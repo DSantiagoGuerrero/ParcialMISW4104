21038625f3007916c39ad4d776d525561e38bfbd
Santiago Guerrero <ds.guerrerom1@uniandes.edu.co>
2025-11-01 13:48:40 -0500

    feat(vehiculo): listado en tabla Bootstrap con totales por marca y encabezado tipo mockup

--- src/app/vehiculo/vehiculo-list/vehiculo-list.component.ts
+++ src/app/vehiculo/vehiculo-list/vehiculo-list.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Vehiculo } from '../vehiculo';
+import { VehiculoService } from '../vehiculo.service';
 
 @Component({
   selector: 'app-vehiculo-list',
-  standalone: true,
-  imports: [],
   templateUrl: './vehiculo-list.component.html',
-  styleUrl: './vehiculo-list.component.scss'
+  styleUrls: ['./vehiculo-list.component.scss']
 })
-export class VehiculoListComponent {
+export class VehiculoListComponent implements OnInit {
+  vehiculos: Vehiculo[] = [];
+  loading = true;
+  error?: string;
 
+  totalesPorMarca: Record<string, number> = {};
+
+  constructor(private vehiculoService: VehiculoService) {}
+
+  ngOnInit(): void {
+    this.vehiculoService.getVehiculos().subscribe({
+      next: data => {
+        this.vehiculos = data;
+        this.totalesPorMarca = this.contarPorMarca(this.vehiculos);
+        this.loading = false;
+      },
+      error: () => { this.error = 'No fue posible cargar los vehículos.'; this.loading = false; }
+    });
+  }
+
+  private contarPorMarca(list: Vehiculo[]): Record<string, number> {
+    return list.reduce((acc, v) => {
+      acc[v.marca] = (acc[v.marca] ?? 0) + 1;
+      return acc;
+    }, {} as Record<string, number>);
+  }
+
+  marcasOrdenadas(): string[] {
+    return Object.keys(this.totalesPorMarca).sort((a, b) => a.localeCompare(b));
+  }
 }
